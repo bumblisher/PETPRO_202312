bad45dd84299b7bd7d48d73034b09e842b87f9b6
bum <kjb6558@naver.com>
2023-12-18 23:58:15 +0900

    컴플리트 저장

--- resource/js/main.js
+++ resource/js/main.js
@@ -4,15 +4,6 @@ $(function(){
         $(this).toggleClass("on");
 		$("html,body").toggleClass("menu_on");
     }); 
-
-	$(".tab_btn button").on("click", function(){
-		var idx = $(this).parent().index();
-
-		$(this).parents(".tab_box").find(".tab_btn button").removeClass("on");
-		$(this).addClass("on");
-		$(this).parents(".tab_box").find(".tab_cont > li").removeClass("on");
-		$(this).parents(".tab_box").find(".tab_cont > li").eq(idx).addClass("on");
-	});
 	
 });
 
